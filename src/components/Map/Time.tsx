1583d4f2450ab702d54102b93fa4b8dd57bfab0d
Tomoro Saito <hg1190159@hiroogakuen.com>
2025-11-01 23:55:03 +0900

    再生と停止に対応

--- src/components/Map/Time.tsx
+++ src/components/Map/Time.tsx
@@ -1,29 +1,46 @@
 import { formatInTimeZone } from "date-fns-tz";
 import { useMap } from "../../context/Map";
 import { useEffect } from "react";
+import {
+  IconPlayerPlay,
+  IconPlayerPlayFilled,
+  IconPlayerStop,
+  IconPlayerStopFilled,
+  IconStepOut,
+} from "@tabler/icons-react";
 
 export default function Time() {
   const {
     clockTheme,
     setClockTheme,
     currentTime,
-
     timeZone,
+    isPaused,
+    setIsPaused,
   } = useMap();
   return (
     <div
-      onClick={() => setClockTheme(clockTheme === "light" ? "dark" : "light")}
-      className="text-white text-sm rounded px-2 py-1 cursor-pointer"
+      className={` text-sm rounded px-2 py-1 cursor-pointer flex items-center ${
+        clockTheme === "dark" && "text-gray-200"
+      } ${
+        clockTheme === "light" && "bg-gray-300 text-white"
+      } rounded-none p-0 m-0`}
     >
       <p
-        className={`code ${clockTheme === "dark" && "text-gray-200"} ${
-          clockTheme === "light" && "bg-gray-300 text-white"
-        }`}
+        className={`code`}
+        onClick={() => setClockTheme(clockTheme === "light" ? "dark" : "light")}
       >
         {formatInTimeZone(currentTime, timeZone, "yyyy-MM-dd")}
         <br />
         {formatInTimeZone(currentTime, timeZone, "HH:mm:ss zzz")}
       </p>
+      <div className="pl-2 pr-1" onClick={() => setIsPaused(!isPaused)}>
+        {isPaused ? (
+          <IconPlayerStopFilled size={25} />
+        ) : (
+          <IconPlayerPlayFilled size={25} />
+        )}
+      </div>
     </div>
   );
 }
